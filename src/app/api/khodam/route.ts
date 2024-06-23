b1df5c37096fe406b1531629ea040a9e7961d118
Eric Daniel <ericdaniel.dev@gmail.com>
2024-06-23 19:00:50 +0700

    Update route.ts

--- src/app/api/khodam/route.ts
+++ src/app/api/khodam/route.ts
@@ -1,6 +1,13 @@
 import getRandom from "@/database/khodam/getRandom";
-
-export async function GET(){
+import { NextResponse } from "next/server";
+export async function GET() {
     const data = await getRandom();
-    return Response.json(data);
-}
+    
+    const response = NextResponse.json(data);
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+    response.headers.set('Surrogate-Control', 'no-store');
+    
+    return response;
+  }
